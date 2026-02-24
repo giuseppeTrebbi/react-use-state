6dbb6b301b49693e12aff4e353f42191d407f56e
giuseppeTrebbi <giuseppe.trebbi97@gmail.com>
2026-02-24 17:28:42 +0100

    feat: project's over

--- src/components/ButtonsList.jsx
+++ src/components/ButtonsList.jsx
@@ -3,7 +3,7 @@ import { languages } from "../assets/languages"
 import { useState } from "react"
 
 export default function ButtonsList() {
-    const [linguaggio, setLinguaggio] = useState({languages})
+    const [linguaggio, setLinguaggio] = useState(languages[0])
     const [attivo, setAttivo] = useState(false)
 
 
@@ -15,7 +15,7 @@ export default function ButtonsList() {
                     languages.map((curLang) => (<button onClick={() => {
                         setLinguaggio(curLang)
                         setAttivo(true)
-                        }} key={curLang.id} type="button" className="btn btn-primary mx-3">
+                        }} key={curLang.id} type="button" className={`btn mx-3 ${curLang.id === linguaggio.id ? "btn-warning" : "btn-primary"}`} >
                             {curLang.title}
                         </button>)
                     )
